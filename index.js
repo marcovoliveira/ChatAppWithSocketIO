5a9ef7133747682d417df6fbef252826ac9b24af
X1 <x1@VOIDs-iMac.local>
2018-07-02 15:50:02 +0100

    socket

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('index'))
+
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.get('/two', function(req, res) {
+
+	res.render('two')
+})
+
+app.get('/three', function(req, res) {
+
+	res.render('three')
+})
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
