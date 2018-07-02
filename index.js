8cd50337b7858bcce72c5c4ae4720692e584a293
X1 <x1@VOIDs-iMac.local>
2018-07-02 17:47:35 +0100

    chat

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+const path = require('path')
+var ejs = require('ejs');
 
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.render('index');
 });
+
+app.get('/two', function(req, res) {
+	res.render('two');
+});
+
+app.get('/three', function(req, res) {
+	res.render('three');
+});
+
 
 io.on('connection', function(socket) {
 	socket.on('chat message', function(msg) {
@@ -13,7 +29,6 @@ io.on('connection', function(socket) {
 	});
 });
 
-
-http.listen(port, function() {
-	console.log('listening on *:' + port);
+http.listen(process.env.PORT || 3000, function() {
+	console.log('app running');
 });
